bc2119e78d17ea379c1a3963efdac29bb5bd9488
hongjian.chen <766256898@qq.com>
2019-01-28 16:38:01 +0800

    完成截取数据并保存到数据库功能

--- src/main/java/com/tpadsz/after/socket/SocketHandler.java
+++ src/main/java/com/tpadsz/after/socket/SocketHandler.java
@@ -5,6 +5,7 @@ import com.tpadsz.after.service.BLTService;
 import com.tpadsz.after.utils.PropertiesUtils;
 import com.tpadsz.after.utils.SpringUtils;
 import org.apache.log4j.Logger;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -13,6 +14,7 @@ import java.io.*;
 import java.net.Socket;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Created by hongjian.chen on 2019/21/1.
@@ -23,8 +25,8 @@ public class SocketHandler implements Runnable {
     private Logger logger = Logger.getLogger(BLTService.class);
     private static final String ROUTING_KEY = PropertiesUtils.getValue("rabbitmq.key");
 
-    //    private static SqlSessionTemplate sqlSessionTemplate = SpringUtils.getSqlSession();
-    private static AmqpTemplate amqpTemplate = SpringUtils.getAmqpTemplate();
+    private static SqlSessionTemplate sqlSessionTemplate = SpringUtils.getSqlSession();
+//    private static AmqpTemplate amqpTemplate = SpringUtils.getAmqpTemplate();
 
     private Socket socket;
 
@@ -60,19 +62,25 @@ public class SocketHandler implements Runnable {
 
 
     public void run() {
-        Map<String, String> map = new HashMap<>();
+//        Map map = new HashMap<>();
         String ip = socket.getInetAddress().getHostAddress();
         int port = socket.getPort();
-        map.put("ip", ip);
+//        map.put("ip", ip);
         logger.info("New connection accepted " + ip + ":" + port);
         BufferedReader br = getReader(socket);
         PrintWriter pw = getWriter(socket);
         String msg;
         try {
             while ((msg = br.readLine()) != null) {
-                logger.info(msg);
-                map.put("msg", msg);
-                amqpTemplate.convertAndSend(ROUTING_KEY, msg);
+                int len = msg.length();
+                if (len >= 36 && len <= 40) {
+                    tempFormat(msg);
+                }
+                if (len > 40) {
+                    formatStr(msg);
+                }
+//                map.put("msg", msg);
+//                amqpTemplate.convertAndSend(ROUTING_KEY, msg);
 //                sqlSessionTemplate.insert("light.insertLog", map);
                 pw.println(echo(msg));
             }
@@ -84,7 +92,78 @@ public class SocketHandler implements Runnable {
             } catch (IOException e) {
                 logger.error(e.getMessage());
             }
-            logger.info("status=" + socket.isClosed());
+            logger.info("isClosed=" + socket.isClosed());
         }
+    }
+
+    private void tempFormat(String format) {
+        String str = format.substring(18, format.length());
+        int len = str.length();
+        String prefix = str.substring(0, 2).toUpperCase();
+        Map map = new ConcurrentHashMap<>();
+        map.put("prefix_value", "03");
+        switch (prefix) {
+            case "52":
+                map.put("ctype", prefix);
+                map.put("cid", str.substring(len - 6, len - 4));
+                break;
+            case "C0":
+                map.put("ctype", prefix);
+                map.put("x", str.substring(4, 6));
+                map.put("y", str.substring(6, 8));
+                break;
+            case "42":
+                map.put("ctype", prefix);
+                map.put("cid", str.substring(len - 4, len - 2));
+                break;
+            default:
+                if ("C1".equals(prefix) || "C4".equals(prefix)) {
+                    map.put("ctype", prefix);
+                    map.put("x", str.substring(2, 4));
+                    map.put("y", str.substring(4, 6));
+                    map.put("cid", str.substring(len - 4, len - 2));
+                }
+                if ("71".equals(prefix)) {
+                    map.put("ctype", prefix);
+                    map.put("x", str.substring(2, 4));
+                    map.put("y", str.substring(4, 6));
+                    map.put("cid", "");
+                }
+                break;
+        }
+        sqlSessionTemplate.selectOne("light.saveConsole", map);
+        logger.info("result=" + map.get("result"));
+    }
+
+    private void formatStr(String str) {
+        Map map = new ConcurrentHashMap<>();
+        String prefix = str.substring(0, 2);
+        map.put("prefix_value", prefix);
+        map.put("dmac", str.substring(2, 14));
+        map.put("mesh_id", str.substring(14, 22));
+        map.put("lmac", str.substring(22, 34));
+        map.put("GID", str.substring(34, 36));
+        switch (prefix) {
+            case "01":
+                map.put("code", str.substring(34, 38));
+                map.put("code_version", str.substring(38, str.length()));
+                break;
+            case "02":
+                map.put("GID", str.substring(34, 36));
+                map.put("x", str.substring(36, 38));
+                map.put("y", str.substring(38, 40));
+                map.put("suffix_value", str.substring(40, str.length()));
+                break;
+            default:
+                if ("03".equals(prefix)) {
+                    map.put("ctype", str.substring(34, 36));
+                    map.put("cid", str.substring(36, 38));
+                    map.put("x", str.substring(38, 40));
+                    map.put("y", str.substring(40, str.length()));
+                }
+                break;
+        }
+        sqlSessionTemplate.selectOne("light.saveConsole", map);
+        logger.info("result=" + map.get("result"));
     }
 }
--- src/main/test/com/tpadsz/after/MyTest.java
+++ src/main/test/com/tpadsz/after/MyTest.java
@@ -96,35 +96,63 @@ public class MyTest {
 
     @Test
     public void testSubstrix() {
+        System.out.println("c0".toUpperCase());
+        String str = "77 04 0F 02 27 35 00 00 00 71 00 13 00 00 00 00 00 00 0E";
+        String c4="77 04 10 02 20 95 00 00 00 C4 5F 02 00 00 00 00 00 00 02 4F".replace(" ","");
+        String str52 = "77 04 10 02 21 69 00 00 00 52 77 65 65 D7 AC F0 00 02 00 85".replace(" ", "");
+        String format = "77 04 0E 02 2A 9D 01 00 00 C0 00 37 37 00 00 00 00 09".replace(" ", "");
+        System.out.println(format + "\t" + format.length());
+        System.out.println("77040F0227250000007132320000000000000D".length());
+        System.out.println("77040F02272500000071020B00000000000004".length());
         String info = "01F0ACD700950108080808F0ACD700920300010304";
         String statusInfo = "02F0ACD700950108080808F0ACD700920301101000";
         String cmd = "03F0ACD700950108080808000000000000c1011010";
-        String prefix = cmd.substring(0, 2);
-        Map<String, Object> map = formatStr(prefix, cmd);
-
-        for (Map.Entry<String, Object> entry : map.entrySet()) {
-            System.out.println("key=" + entry.getKey() + ",value=" + entry.getValue());
-        }
+        String prefix = format.substring(18, 20);
+        Map<String, String> map = tempFormat(prefix, format.substring(18, format.length()));
+//        for (Map.Entry<String, String> entry : map.entrySet()) {
+//            System.out.println("key=" + entry.getKey() + ",value=" + entry.getValue());
+//        }
     }
 
-    public Map<String, Object> formatStr(String prefix, String str) {
-        Map<String, Object> map = new ConcurrentHashMap<>();
+    public Map tempFormat(String prefix, String str) {
+        int len = str.length();
+        Map<String, String> map = new ConcurrentHashMap<>();
+        map.put("prefix_value", "03");
+        switch (prefix) {
+            case "52":
+                map.put("ctype", prefix);
+                map.put("cid", str.substring(len - 6, len - 4));
+                break;
+            case "C0":
+                map.put("ctype", prefix);
+                map.put("x", str.substring(4, 6));
+                map.put("y", str.substring(6, 8));
+                break;
+            case "42":
+                map.put("ctype", prefix);
+                map.put("cid", str.substring(len - 4, len - 2));
+                break;
+            default:
+                map.put("ctype", prefix);
+                map.put("x", str.substring(2, 4));
+                map.put("y", str.substring(4, 6));
+                if ("71".equals(prefix)) {
+                    map.put("cid", "");
+                } else {
+                    map.put("cid", str.substring(len - 4, len - 2));
+                }
+                break;
+        }
+        return map;
+    }
 
+    public Map formatStr(String prefix, String str) {
+        Map map = new ConcurrentHashMap<>();
         map.put("prefix_value", str.substring(0, 2));
         map.put("dmac", str.substring(2, 14));
         map.put("mesh_id", str.substring(14, 22));
         map.put("lmac", str.substring(22, 34));
-//        map.put("code", "");
-//        map.put("code_version", "");
-//        map.put("GID", "");
-//        map.put("x", "");
-//        map.put("y", "");
-//        map.put("ctype", "");
-//        map.put("cid", "");
-//        map.put("suffix_value", "");
-//        map.put("result", 0);
-
-        System.out.println(map.size());
+        map.put("GID", str.substring(34, 36));
         switch (prefix) {
             case "01":
                 map.put("code", str.substring(34, 38));
@@ -164,10 +192,16 @@ public class MyTest {
     public void testDB() {
         SqlSessionTemplate sqlSessionTemplate = SpringUtils.getSqlSession();
 //        System.out.println(sqlSessionTemplate.selectList("light.getLights").size());
+//        String 71 = "77040F0227350000007100130000000000000E";
+        String c4="77 04 10 02 20 95 00 00 00 C4 5F 02 00 00 00 00 00 00 02 4F".replace(" ","");
+        String c1="77 04 10 02 20 9D 01 00 00 C1 32 32 00 00 00 00 00 00 02 1E".replace(" ","");
+        String str="77 04 0E 02 2A 9D 01 00 00 C0 00 37 37 00 00 00 00 09".replace(" ","");
         String info = "01F0ACD700950108080808F0ACD700920300010304";
         String statusInfo = "02F0ACD700950108080808F0ACD700920301101000";
         String cmd = "03F0ACD700950108080808000000000000c1011010";
-        Map pram = formatStr("01", info);
+//        Map pram = formatStr("01", info);
+        String prefix = str.substring(18, 20);
+        Map pram = tempFormat(prefix, str.substring(18, str.length()));
         sqlSessionTemplate.selectOne("light.saveConsole", pram);
         System.out.println("result=" + pram.get("result"));
     }
